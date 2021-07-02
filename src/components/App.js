2263dafd9682817e1205e0122d0c061c87833229
Antonio <globaltailwind@protonmail.com>
2021-07-02 16:25:13 -0300

    push here

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import CoinApp from './CoinApp';
-
+import Form from '../Form';
 function App() {
   return (
     
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: '400px' }}>
-       
+       <Form/>
           <CoinApp />
           
         </div>
